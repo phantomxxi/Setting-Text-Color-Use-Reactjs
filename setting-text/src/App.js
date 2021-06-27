bfe3e23b64bc476e39c57bd5c0335102325c6a47
phantomXXI <haianhlol2001@gmail.com>
2021-06-27 15:52:19 +0700

    Phân chia Component

---
+++ setting-text/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./App.css";
+import ColorPicker from "./components/ColorPicker";
+import SizeSetting from "./components/SizeSetting";
+import Reset from "./components/Reset";
+import Result from "./components/Result";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container mt-50">
+        <div className="row">
+          <ColorPicker />
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <SizeSetting />
+            <Reset />
+          </div>
+          <Result />
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ setting-text/src/components/Result.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Result extends Component {
+  render() {
+    return (
+      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <p> Color: red - Font - size: 15 px </p>{" "}
+        <div id="content"> Nội dung setting </div>{" "}
+      </div>
+    );
+  }
+}
+export default Result;
